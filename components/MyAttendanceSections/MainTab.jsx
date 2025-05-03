b7a7733796d897632034c4a41a140104d1495366
shruti misal <shrutimisal109@gmail.com>
2025-05-03 18:02:48 +0530

    attendance page navigation

--- components/MyAttendanceSections/MainTab.jsx
+++ components/MyAttendanceSections/MainTab.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./maintab.module.css";
 import { MdBadge } from "react-icons/md";
+import { IoPersonCircle } from "react-icons/io5";
+import AttendanceSection from "./AttendanceSection";
 
 const MainTab = () => {
   const name = "Shruti Misal";
   const isActive = true;
-  const empId = "EV233";
+  const empId = "EV";
   const desg = "App Developer";
+
+  const [selectedTab, setSelectedTab] = useState("Attendance");
+
+  const renderSection = () => {
+    switch (selectedTab) {
+      case "Personal":
+        return <AttendanceSection />;
+      case "Attendance":
+        return <AttendanceSection />;
+      case "Leave":
+        return <AttendanceSection />;
+      case "Weekoff":
+        return <AttendanceSection />;
+      case "Regularization":
+        return <AttendanceSection />;
+      case "Gracetime":
+        return <AttendanceSection />;
+      case "Reimbursement":
+        return <AttendanceSection />;
+      case "Assets":
+        return <AttendanceSection />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <div className={styles.wholeContainer}>
@@ -29,25 +56,68 @@ const MainTab = () => {
           </div>
         </div>
         <div>
-          <div className={styles.empid}>Emp Id : {empId}</div>
+          <div className={styles.empid}>
+            <IoPersonCircle size={15} color="#d3af1f"/> Emp Id : {empId}
+          </div>
           <div className={styles.desgination}>
-            <MdBadge />
+            <MdBadge size={15} color="#d3af1f"/>
             {desg}
           </div>
         </div>
       </div>
       <div className={styles.navbar}>
         <div className={styles.links}>
-          <span>Personal Information</span>
-          <span>Attendance</span>
-          <span>Leave</span>
-          <span>Week off</span>
-          <span>Regularization</span>
-          <span>Grace Time</span>
-          <span>Reimbursement</span>
-          <span>Assets</span>
+          <span
+            onClick={() => setSelectedTab("Personal")}
+            className={selectedTab === "Personal" ? styles.activeTab : ""}
+          >
+            Personal Information
+          </span>
+          <span
+            onClick={() => setSelectedTab("Attendance")}
+            className={selectedTab === "Attendance" ? styles.activeTab : ""}
+          >
+            Attendance
+          </span>
+          <span
+            onClick={() => setSelectedTab("Leave")}
+            className={selectedTab === "Leave" ? styles.activeTab : ""}
+          >
+            Leave
+          </span>
+          <span
+            onClick={() => setSelectedTab("Weekoff")}
+            className={selectedTab === "Weekoff" ? styles.activeTab : ""}
+          >
+            Week off
+          </span>
+          <span
+            onClick={() => setSelectedTab("Regularization")}
+            className={selectedTab === "Regularization" ? styles.activeTab : ""}
+          >
+            Regularization
+          </span>
+          <span
+            onClick={() => setSelectedTab("Gracetime")}
+            className={selectedTab === "Gracetime" ? styles.activeTab : ""}
+          >
+            Grace Time
+          </span>
+          <span
+            onClick={() => setSelectedTab("Reimbursement")}
+            className={selectedTab === "Reimbursement" ? styles.activeTab : ""}
+          >
+            Reimbursement
+          </span>
+          <span
+            onClick={() => setSelectedTab("Assets")}
+            className={selectedTab === "Assets" ? styles.activeTab : ""}
+          >
+            Assets
+          </span>
         </div>
       </div>
+      <div className={styles.sectionContainer}>{renderSection()}</div>
     </div>
   );
 };
