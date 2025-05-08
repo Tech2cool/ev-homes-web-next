ffef79d6a9e357bff2fd9e560747194e3f664781
shruti misal <shrutimisal109@gmail.com>
2025-05-08 14:32:13 +0530

    leave redesign

--- components/MyAttendanceSections/LeaveSection.js
+++ components/MyAttendanceSections/LeaveSection.js
@@ -1,228 +1,231 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useState } from "react";
 import styles from "./leavesection.module.css";
 import { PiAirplaneTakeoffFill } from "react-icons/pi";
-import CustomSelect from "../CustomSelect";
+import { LuAlarmClock } from "react-icons/lu";
 import { FaBagShopping } from "react-icons/fa6";
-import {
-  FaCalendarCheck,
-  FaCalendarDay,
-  FaDyalog,
-  FaFileUpload,
-  FaUser,
-} from "react-icons/fa";
+import { MdOutlineFeedback, MdOutlineCallToAction } from "react-icons/md";
+import { FaCalendarCheck, FaCalendarDay, FaFileUpload } from "react-icons/fa";
+import { FaPersonWalkingLuggage } from "react-icons/fa6";
 
-const leaveDate = [
+const initialLeaveData = [
   {
     apply: "25 April 2025",
     startdate: "28 April 2025",
-    EndDate: "29 April 2025",
-    TotalDays: "2",
-    LeaveType: "Paid Leave",
-    Reason: "personal reson..",
+    enddate: "29 April 2025",
+    totalDays: "2",
+    leaveType: "Casual Leave",
+    reason: "Personal reason..",
     status: "Pending",
-    color: "#FF9800",
+    attachment: "",
   },
   {
     apply: "12 March 2025",
     startdate: "15 March 2025",
-    EndDate: "20 March 2025",
-    TotalDays: "6",
-    LeaveType: "Paid Leave",
-    Reason: "internal exam..",
+    enddate: "20 March 2025",
+    totalDays: "6",
+    leaveType: "Comp Leave",
+    reason: "Internal exam...",
     status: "Approved",
-    color: "#4CAF50",
+    attachment: "",
   },
   {
     apply: "02 Feb 2025",
     startdate: "05 Feb 2025",
-    EndDate: "10 Feb 2025",
-    TotalDays: "6",
-    LeaveType: "Paid Leave",
-    Reason: "For Personal..",
+    enddate: "10 Feb 2025",
+    totalDays: "6",
+    leaveType: "Paid Leave",
+    reason: "For personal...",
     status: "Rejected",
-    color: "#F44336",
+    attachment: "",
+  },
+  {
+    apply: "02 Feb 2025",
+    startdate: "05 Feb 2025",
+    enddate: "10 Feb 2025",
+    totalDays: "6",
+    leaveType: "Paid Leave",
+    reason: "For personal...",
+    status: "Rejected",
+    attachment: "",
   },
 ];
 
 export default function LeaveSection() {
-  const [startdate, setstartdate] = useState("");
-  const [enddate, setenddate] = useState("");
-  const [numberleave, setnumberleave] = useState("");
-  const [reason, setreason] = useState("");
+  const [leaveData, setLeaveData] = useState(initialLeaveData);
+  const [filter, setFilter] = useState("All");
 
-  const fileInputRef = useRef(null);
-  const [file, setFile] = useState(null);
+  const filteredData =
+    filter == "All"
+      ? leaveData
+      : leaveData.filter((leave) => leave.status === filter);
 
-  const startDateRef = useRef(null);
-  const endDateRef = useRef(null);
+
+  const getTotalDays = (type) =>
+    leaveData
+      .filter((l) => l.leaveType === type)
+      .reduce((sum, l) => sum + parseInt(l.totalDays, 10), 0);
+
+  const casualCount = getTotalDays("Casual Leave");
+  const compCount = getTotalDays("Comp Leave");
+  const paidCount = getTotalDays("Paid Leave");
+
+  const totalDaysAll = casualCount + compCount + paidCount;
+
+  const casualPercent = totalDaysAll ? (casualCount / totalDaysAll) * 100 : 0;
+  const compPercent = totalDaysAll ? (compCount / totalDaysAll) * 100 : 0;
+  const paidPercent = totalDaysAll ? (paidCount / totalDaysAll) * 100 : 0;
 
   return (
     <div className={styles.maincontainer}>
       <div className={styles.leaveSection}>
-        <div className={styles.leavecontainer}>
-          <div className={styles.leavecard}>
-            <p className={styles.request}>Requested</p>
-            <div className={styles.numberleave}>4</div>
+        <div className={styles.statsWithProgress}>
+          <div className={styles.leavecontainer}>
+            <div className={styles.leavecard} onClick={() => setFilter("All")}>
+              <div className={styles.request}>Requested</div>
+              <div className={styles.numberleave}>{leaveData.length}</div>
+            </div>
+            <div
+              className={styles.leavecard}
+              onClick={() => setFilter("Approved")}
+            >
+              <div className={styles.aproved}>Approved</div>
+              <div className={styles.numberleave}>
+                {leaveData.filter((l) => l.status === "Approved").length}
+              </div>
+            </div>
+            <div
+              className={styles.leavecard}
+              onClick={() => setFilter("Rejected")}
+            >
+              <div className={styles.rejected}>Rejected</div>
+              <div className={styles.numberleave}>
+                {leaveData.filter((l) => l.status === "Rejected").length}
+              </div>
+            </div>
+            <div
+              className={styles.leavecard}
+              onClick={() => setFilter("Pending")}
+            >
+              <div className={styles.pending}>Pending</div>
+              <div className={styles.numberleave}>
+                {leaveData.filter((l) => l.status === "Pending").length}
+              </div>
+            </div>
           </div>
-          <div className={styles.divider}></div>
-          <div className={styles.leavecard}>
-            <p className={styles.aproved}>Approved</p>
-            <div className={styles.numberleave}>2</div>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.leavecard}>
-            <p className={styles.rejected}>Rejected</p>
-            <div className={styles.numberleave}>1</div>
-          </div>
-          <div className={styles.divider}></div>
-          <div className={styles.leavecard}>
-            <p className={styles.pending}>Pending</p>
-            <div className={styles.numberleave}>1</div>
-          </div>
-        </div>
 
-        <div className={styles.container}>
-          {leaveDate.map((Section, i) => (
-            <div key={i} className={styles.info}>
-              <div className={styles.vector}>
-                <div className={styles.iconplan}>
-                  <PiAirplaneTakeoffFill style={{ color: Section.color }} />
+          <div className={styles.progressContainer}>
+            <div className={styles.leaveHeadline}>
+              <LuAlarmClock /> Your Leave
+            </div>
+            <div className={styles.progressGroup}>
+              <div className={styles.labelColumn}>
+                <span>Casual Leave</span>
+                <span>Comp Off</span>
+                <span>Paid Leave</span>
+              </div>
+              <div className={styles.barColumn}>
+                <div className={styles.progressBar}>
+                  <div
+                    className={styles.progressFillCL}
+                    style={{ width: `${casualPercent}%` }}
+                  />
+                </div>
+                <div className={styles.progressBar}>
+                  <div
+                    className={styles.progressFillCO}
+                    style={{ width: `${compPercent}%` }}
+                  />
+                </div>
+                <div className={styles.progressBar}>
+                  <div
+                    className={styles.progressFillPL}
+                    style={{ width: `${paidPercent}%` }}
+                  />
                 </div>
               </div>
-              <div className={styles.context}>
-                <h6>Apply</h6>
-                <p>{Section.apply}</p>
+              <div className={styles.countColumn}>
+                <span>{casualCount.toString().padStart(2, "0")}</span>
+                <span>{compCount.toString().padStart(2, "0")}</span>
+                <span>{paidCount.toString().padStart(2, "0")}</span>
               </div>
-              <div className={styles.context}>
-                <h6>Start Date</h6>
-                <p>{Section.startdate}</p>
-              </div>
-              <div className={styles.context}>
-                <h6>End Date</h6>
-                <p>{Section.EndDate}</p>
-              </div>
-              <div className={styles.context}>
-                <h6>Total Days</h6>
-                <p>{Section.TotalDays}</p>
-              </div>
-              <div className={styles.context}>
-                <h6>Leave Type</h6>
-                <p>{Section.LeaveType}</p>
-              </div>
-              <div className={styles.context}>
-                <h6>Reason</h6>
-                <p>{Section.Reason}</p>
-              </div>
-              <div className={styles.context}>
-                <h4 style={{ color: Section.color }}>{Section.status}</h4>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className={styles.applysection}>
-        <div className={styles.formHeadline}>Leave Application Form</div>
-
-        <div className={styles.formControl}>
-          <label htmlFor="select">
-            Leave Type <span style={{ color: "red" }}>*</span>
-          </label>
-          <select name="select" id="select">
-            <option value="first-call">Paid Leave</option>
-            <option value="followup">Casual Leave</option>
-            <option value="schedule-meeting">Compensation Leave</option>
-          </select>
-        </div>
-
-        {/* Start Date */}
-        <div
-          className={styles.formControl}
-          onClick={() => startDateRef.current?.showPicker()}
-        >
-          <label htmlFor="startdate">
-            Start Date <span style={{ color: "red" }}>*</span>
-          </label>
-          <FaCalendarDay className={styles.dateicon} />
-          <input
-            type="date"
-            ref={startDateRef}
-            id="startdate"
-            value={startdate}
-            onChange={(e) => setstartdate(e.target.value)}
-          />
-        </div>
-
-        {/* End Date */}
-        <div
-          className={styles.formControl}
-          onClick={() => endDateRef.current?.showPicker()}
-        >
-          <label htmlFor="enddate">
-            End Date <span style={{ color: "red" }}>*</span>
-          </label>
-
-          <input
-            type="date"
-            ref={endDateRef}
-            id="enddate"
-            value={enddate}
-            onChange={(e) => setenddate(e.target.value)}
-          />
-          <FaCalendarDay className={styles.dateicon} />
-        </div>
-
-        <div className={styles.formControl}>
-          <label htmlFor="numberleave">
-            Number of Day <span style={{ color: "red" }}>*</span>
-          </label>
-          <input
-            type="text"
-            id="numberleave"
-            value={numberleave}
-            onChange={(e) => setnumberleave(e.target.value)}
-            placeholder="Enter number of days"
-          />
-        </div>
-
-        <div className={styles.formControl}>
-          <input
-            type="file"
-            id="attachment"
-            ref={fileInputRef}
-            onChange={(e) => setFile(e.target.files[0])}
-            style={{ display: "none" }}
-          />
-
-          <div
-            className={styles.uploadBox}
-            onClick={() => fileInputRef.current.click()}
-          >
-            <FaFileUpload className={styles.inputIcon} />
-            <div className={styles.textContainer}>
-              <span className={styles.uploadText}>Upload File</span>
             </div>
           </div>
-
-          {file && <span className={styles.fileName}>{file.name}</span>}
         </div>
 
-        <div className={styles.formControl}>
-          <label htmlFor="reason">
-            Reason <span style={{ color: "red" }}>*</span>
-          </label>
-          <textarea
-            id="reason"
-            value={reason}
-            onChange={(e) => setreason(e.target.value)}
-            placeholder="Enter your reason"
-            className={styles.input}
-            rows={4}
-          />
+        <div className={styles.tableContainer}>
+          <div className={styles.topBar}>
+            <div className={styles.applyButton}>
+              <FaPersonWalkingLuggage className={styles.applyIcon} /> Apply
+              Leave
+            </div>
+          </div>
+          <table className={styles.leaveTable}>
+            <thead>
+              <tr>
+                <th>
+                  <FaBagShopping /> Applied On
+                </th>
+                <th>
+                  <FaCalendarDay /> Start Date & End Date
+                </th>
+                <th>
+                  <FaCalendarCheck /> Total days
+                </th>
+                <th>
+                  <PiAirplaneTakeoffFill /> Leave Type
+                </th>
+                <th>
+                  <MdOutlineFeedback /> Reason
+                </th>
+                <th>
+                  <FaFileUpload /> Attachment
+                </th>
+                <th>
+                  <MdOutlineCallToAction /> Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((leave, index) => (
+                <tr key={index}>
+                  <td>{leave.apply}</td>
+                  <td>
+                    {leave.startdate} to {leave.enddate}
+                  </td>
+                  <td>{leave.totalDays}</td>
+                  <td>{leave.leaveType}</td>
+                  <td>{leave.reason}</td>
+                  <td>
+                    {leave.attachment ? (
+                      <a
+                        href={leave.attachment}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        View
+                      </a>
+                    ) : (
+                      "-"
+                    )}
+                  </td>
+                  <td>
+                    <span
+                      className={
+                        leave.status === "Approved"
+                          ? styles.statusApproved
+                          : leave.status === "Rejected"
+                          ? styles.statusRejected
+                          : styles.statusPending
+                      }
+                    >
+                      {leave.status}
+                    </span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        {/* <div className={styles.button}>
-          <button className={styles.submitButton}>Submit</button>
-        </div> */}
       </div>
     </div>
   );
